468397a6cfba4743b26bf295beabfccda01fb6e1
Shlomo spaeth <sam.spaeth@gmail.com>
2022-06-12 22:37:20 -0400

    updated the grouping for the year. This year a factor or remote and in person judges added another variable to the breakdown.

--- public/json/Groups.js
+++ public/json/Groups.js
@@ -1,15 +1,117 @@
 const group1 = [
   {
+    letter: "A2",
+    type: "Professional still media section",
+    subtype: "Didactic/Instructional ",
+    specific: "- Commercial",
+  },
+  {
     letter: "B",
     type: "Professional still media section",
     subtype: "still media",
     specific: " - Editorial",
   },
   {
+    letter: "K",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Advertising and Marketing/Promotional",
+  },
+  {
+    letter: "I1",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Anatomical/ Pathological",
+  }
+];
+
+const group2 = [
+  {
     letter: "C",
     type: "Professional still media section",
     subtype: "Still Media ",
     specific: "- Advertising and Marketing/Promotional",
+  },
+  {
+    letter: "D",
+    type: "Professional still media section",
+    subtype: "Still Media ",
+    specific: "- Medical Legal",
+  },
+  {
+    letter: "E",
+    type: "Professional still media section",
+    subtype: "Illustrated Textbook",
+    specific: " - Traditionally printed book",
+  },
+  {
+    letter: "H",
+    type: "Professional Dimensional Media",
+    subtype: "Simulators, Prosthetics and Sculptural Products",
+    specific: "Simulators, prostheses or other tactile three-dimensional forms ",
+  },
+  {
+    letter: "I3",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Molecular/Biological/Life Sciences",
+  },
+  {
+    letter: "J",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "- Editorial",
+  }
+];
+
+const group3 = [
+  {
+    letter: "A1",
+    type: "Professional still media section",
+    subtype: "Didactic/Instructional ",
+    specific: "- Non-Commercial",
+  },
+  {
+    letter: "I2",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Surgical/Clinical Procedures",
+  }
+];
+
+const group4 = [
+  {
+    letter: "F1",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Didactic/Instructional – Non-Commercial",
+  },
+  {
+    letter: "F2",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Animation-Didactic/Instructional - Commercial",
+  },
+  {
+    letter: "F3",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: " Advertising and Marketing/Promotional",
+  }
+];
+
+const group5 = [
+  {
+    letter: "L",
+    type: "Student Motion Media ",
+    subtype: "Motion",
+    specific: "– Animation",
+  },
+  {
+    letter: "M",
+    type: "Student Interactive Section",
+    subtype: "Media",
+    specific: "Interactive",
   },
   {
     letter: "G1",
@@ -37,119 +139,6 @@ const group1 = [
   }
 ];
 
-const group2 = [
-  {
-    letter: "A1",
-    type: "Professional still media section",
-    subtype: "Didactic/Instructional ",
-    specific: "- Non-Commercial",
-  },
-  {
-    letter: "A2",
-    type: "Professional still media section",
-    subtype: "Didactic/Instructional ",
-    specific: "- Commercial",
-  }
-];
-
-const group3 = [
-  {
-    letter: "F1",
-    type: "Professional Motion Media",
-    subtype: "Animation",
-    specific: " Didactic/Instructional – Non-Commercial",
-  },
-  {
-    letter: "F2",
-    type: "Professional Motion Media",
-    subtype: "Animation",
-    specific: " Animation-Didactic/Instructional - Commercial",
-  },
-  {
-    letter: "F3",
-    type: "Professional Motion Media",
-    subtype: "Animation",
-    specific: " Advertising and Marketing/Promotional",
-  }
-];
-
-const group4 = [
-  {
-    letter: "I3",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "– Molecular/Biological/Life Sciences",
-  },
-  {
-    letter: "L",
-    type: "Student Motion Media ",
-    subtype: "Motion",
-    specific: "– Animation",
-  },
-  {
-    letter: "M",
-    type: "Student Interactive Section",
-    subtype: "Media",
-    specific: "Interactive",
-  }
-];
-
-const group5 = [
-  {
-    letter: "D",
-    type: "Professional still media section",
-    subtype: "Still Media ",
-    specific: "- Medical Legal",
-  },
-  {
-    letter: "F4",
-    type: "Professional Motion Media",
-    subtype: "Animation",
-    specific: "Medical-Legal",
-  },
-  {
-    letter: "I2",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "– Surgical/Clinical Procedures",
-  }
-];
-
-const group6 = [
-  {
-    letter: "I1",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "– Anatomical/ Pathological",
-  },
-  {
-    letter: "J",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "- Editorial",
-  },
-  {
-    letter: "K",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "– Advertising and Marketing/Promotional",
-  }
-];
-
-const group7 = [
-  {
-    letter: "E",
-    type: "Professional still media section",
-    subtype: "Illustrated Textbook",
-    specific: " - Traditionally printed book",
-  },
-  {
-    letter: "G3",
-    type: "Professional Motion Media",
-    subtype: "Interactive Media",
-    specific: " - Advertising and Marketing/Promotional",
-  }
-];
 
 const groupAdmin = [
   {
@@ -183,6 +172,12 @@ const groupAdmin = [
     specific: "- Medical Legal",
   },
   {
+    letter: "E",
+    type: "Professional still media section",
+    subtype: "Illustrated Textbook",
+    specific: " - Traditionally printed book",
+  },
+  {
     letter: "F1",
     type: "Professional Motion Media",
     subtype: "Animation",
@@ -219,6 +214,12 @@ const groupAdmin = [
     specific: " - Didactic/Instructional-Commercial",
   },
   {
+    "letter": "G3",
+    "type": "Professional Motion Media",
+    "subtype": "Interactive Media",
+    "specific": " - Advertising and Marketing/Promotional"
+  },
+  {
     letter: "G4",
     type: "Professional Motion Media",
     subtype: "Interactive Media",
@@ -231,10 +232,10 @@ const groupAdmin = [
     specific: "Interactive Media – Interactive Textbook",
   },
   {
-    letter: "M",
-    type: "Student Interactive Section",
-    subtype: "Media",
-    specific: "Interactive",
+    letter: "H",
+    type: "Professional Dimensional Media",
+    subtype: "Simulators, Prosthetics and Sculptural Products",
+    specific: "Simulators, prostheses or other tactile three-dimensional forms ",
   },
   {
     letter: "I1",
@@ -243,16 +244,16 @@ const groupAdmin = [
     specific: "– Anatomical/ Pathological",
   },
   {
-    letter: "L",
-    type: "Student Motion Media ",
-    subtype: "Motion",
-    specific: "– Animation",
-  },
-  {
     letter: "I2",
     type: "Student Still Media Section",
     subtype: "Didactic/Instructional",
     specific: "– Surgical/Clinical Procedures",
+  },
+  {
+    letter: "I3",
+    type: "Student Still Media Section",
+    subtype: "Didactic/Instructional",
+    specific: "– Molecular/Biological/Life Sciences",
   },
   {
     letter: "J",
@@ -267,30 +268,37 @@ const groupAdmin = [
     specific: "– Advertising and Marketing/Promotional",
   },
   {
-    letter: "I3",
-    type: "Student Still Media Section",
-    subtype: "Didactic/Instructional",
-    specific: "– Molecular/Biological/Life Sciences",
+    letter: "L",
+    type: "Student Motion Media ",
+    subtype: "Motion",
+    specific: "– Animation",
   },
   {
-    letter: "E",
-    type: "Professional still media section",
-    subtype: "Illustrated Textbook",
-    specific: " - Traditionally printed book",
+    letter: "M",
+    type: "Student Interactive Section",
+    subtype: "Media",
+    specific: "Interactive",
+  }
+];
+
+const emptyCategories = [
+  {
+    letter: "F4",
+    type: "Professional Motion Media",
+    subtype: "Animation",
+    specific: "Medical-Legal",
   },
   {
-    letter: "G3",
+    letter: "G5",
     type: "Professional Motion Media",
     subtype: "Interactive Media",
-    specific: " - Advertising and Marketing/Promotional",
-  },
-];
+    specific: "Interactive Media – Interactive Textbook",
+  }
+]
 
 module.exports.group1 = group1;
 module.exports.group2 = group2;
 module.exports.group3 = group3;
 module.exports.group4 = group4;
 module.exports.group5 = group5;
-module.exports.group6 = group6;
-module.exports.group7 = group7;
 module.exports.groupAdmin = groupAdmin;
